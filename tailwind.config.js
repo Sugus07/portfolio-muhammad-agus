46f04426b2a9b87298fbdf6b83b5f325d406b379
MUHAMMAD AGUS <muhammadagus070800@gmail.com>
2023-05-21 22:32:35 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors : {
+        'primary' : '#14b8a6',
+        'secondary' : '#64748b',
+        'dark' : '#0f172a',
+        'nusa' : '#5F264A',
+        'active' : '#0d9488',
+      }
+    },
+  },
+  plugins: [],
+}
+
